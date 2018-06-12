ad04fc0d56557af49c293dcd2a50fa012b8ec279
Kevin Duraj <kevin@pacificair.com>
2018-06-12 14:10:10 -0700

    Update App.java

--- src/searchengine/App.java
+++ src/searchengine/App.java
@@ -11,16 +11,16 @@ public class App {
 
         BTree<String, String> index = new BTree<>();
 
-        index.put("apple"     , "10000000");  // indexing term in document 1
-        index.put("orange"    , "01000000");  // indexing term in document 2
-        index.put("cucumber"  , "00100000");  // indexing term in document 3
-        index.put("carrot"    , "00010000");  // indexing term in document 4
-        index.put("artichokes", "00010000");  // indexing term in document 4
+        index.put("apple"    , "10000000");  // indexing term in document 1
+        index.put("orange"   , "01000000");  // indexing term in document 2
+        index.put("cucumber" , "00100000");  // indexing term in document 3
+        index.put("carrot"   , "00010000");  // indexing term in document 4
+        index.put("artichoke", "00010000");  // indexing term in document 4
 
-        System.out.println("apple document offset      : " + index.get("apple"));
-        System.out.println("orange document offset     : " + index.get("orange"));
-        System.out.println("carrot document offset     : " + index.get("carrot"));
-        System.out.println("artichokes document offset : " + index.get("artichokes"));
+        System.out.println("apple document offset     : " + index.get("apple"));
+        System.out.println("orange document offset    : " + index.get("orange"));
+        System.out.println("carrot document offset    : " + index.get("carrot"));
+        System.out.println("artichoke document offset : " + index.get("artichokes"));
 
         System.out.println();
 
