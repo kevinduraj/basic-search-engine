4531078515455e4e7712fe29eda13d1e3e996e92
Kevin Duraj <kevin@pacificair.com>
2018-06-12 14:16:20 -0700

    Update App.java

--- src/searchengine/App.java
+++ src/searchengine/App.java
@@ -12,11 +12,16 @@ public class App {
         BTree<String, String> index = new BTree<>();
 
         index.put("apple"    , "10000000");  // indexing term in document 1
+        index.put("broccoli" , "10000000");  // indexing term in document 1
         index.put("orange"   , "01000000");  // indexing term in document 2
-        index.put("cucumber" , "00100000");  // indexing term in document 3
+        index.put("cucumber" , "00100000");  // indexing term in document 3  
         index.put("carrot"   , "00010000");  // indexing term in document 4
         index.put("artichoke", "00010000");  // indexing term in document 4
-
+        
+        // Because we add broccoli in document 1 and now at document 5 Bitmap signarure changes
+        // These changes cause indexing to slow down as more documents with the same terms are added
+        index.put("broccoli",  "10001000");  // indexing term in document 5
+        
         System.out.println("apple document offset     : " + index.get("apple"));
         System.out.println("orange document offset    : " + index.get("orange"));
         System.out.println("carrot document offset    : " + index.get("carrot"));
